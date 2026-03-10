7b5c606c54bca82cbecd2526df227e5ab4c2938b
KS1 <shinehome888@gmail.com>
2026-03-10 01:44:53 -0700

    auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const { register, login } = require('../controllers/auth.controller');
+const { protect } = require('../middleware/auth.middleware');
 
 const router = express.Router();
+
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', protect, async (req, res) => {
+  res.json(req.user);
+});
+
 module.exports = router;
